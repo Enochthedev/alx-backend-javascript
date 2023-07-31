d90fe36a47754b71e6547025b20b9fa401f15486
Toluthedev <enochjesse884@gmail.com>
2023-07-31 22:50:03 +0100

    Add files via upload

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Omosebi Toluwani enoch <https://github.com/Enochthedev>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Omosebi Toluwani enoch <https://github.com/Enochthedev>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
